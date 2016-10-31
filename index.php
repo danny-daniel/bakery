6849c1ac194352aabc53ec442a0df7b6ce67b906
Sonali Bharat <sonalibharat17@yahoo.com>
2016-10-31 23:36:36 +0000

    img file, css file, and main page

---
+++ index.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Conchitas! </title>
+        
+            <link rel="stylesheet" href="css/project1.css" type="text/css" />
+    </head>
+    <body>
+        <h1> <b> Welcome! </b></h1>
+    </body>
+</html>
