47c8b417a4e6177460aec5e2b09be9cfe56a71c9
Sonali Bharat <sonalibharat17@yahoo.com>
2016-10-31 23:56:07 +0000

    constructed the css for the main page and implemented the About Us, Location and Menu page

--- index.php
+++ index.php
@@ -6,6 +6,18 @@
             <link rel="stylesheet" href="css/project1.css" type="text/css" />
     </head>
     <body>
+        
         <h1> <b> Welcome! </b></h1>
+        
+        
+         <nav>
+       <a href="menu.php"  >Menu </a>
+       &emsp;
+       <a href="location.php" > Location </a>
+       &emsp;
+       <a href="aboutUs.php" > About Us </a>
+       
+    </nav>
+    
     </body>
 </html>
