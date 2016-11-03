c3e1475ce49628c14c7f4b76c6ca003ff062720a
Daniel Mora <dmora@csumb.edu>
2016-11-03 20:23:31 +0000

    Added pan dulce to menu, added user story to about

--- aboutUs.php
+++ aboutUs.php
@@ -2,8 +2,9 @@
 <html>
     <head>
         <title>About Us </title>
+        <link rel="stylesheet" href="css/project1.css" type="text/css" />
     </head>
     <body>
-
+        <a href="https://docs.google.com/a/csumb.edu/document/d/15Y2a8pX6osQWlWS9bxPRVzYsgM_ioskDdpTHeaWm_MQ/edit?usp=sharing">Our Story</a>
     </body>
 </html>
--- menu.php
+++ menu.php
@@ -1,9 +1,58 @@
+<?php
+    include("../../includes/dbConnection.php");
+    session_start();
+    
+    $dbConn = getDatabaseConnection('bakery');
+    
+    function getPanDulce()
+    {
+        global $dbConn;
+        
+        $sql = "SELECT * FROM
+            Bread ORDER BY bread";
+            
+        $statement = $dbConn->prepare($sql);
+        $statement->execute();
+        $pan = $statement->fetchAll(PDO::FETCH_ASSOC);
+        
+        echo "<form>";
+        echo "<table>";
+        
+        foreach ($pan as $bread)
+        {
+            echo "<tr><td><input type='checkbox' name='cart[]'></td>";
+            echo "<td>" .$bread['bread']. "</td> <td>" .$bread['price']. "</td></tr>";
+        }
+        
+        echo "</table>";
+        echo "</form>";
+    }
+?>
+
 <!DOCTYPE html>
 <html>
     <head>
         <title> Menu </title>
+        <link rel="stylesheet" href="css/project1.css" type="text/css" />
     </head>
     <body>
-
+        <form>
+            Please choose from our wide selection: <select name="option">
+                <option>Choose one</option>
+                <option>Pan Dulce</option>
+                <option>Drinks</option>
+                <option>Pastries</option>
+                <option>Sandwiches</option>
+                <option>Vegetarian</option>
+            </select>
+            
+            <input type='submit' name='submitRequest' value="Get Foods">
+            
+        </form>
+        
+        <?php
+            if (isset($_GET['submitRequest']) && $_GET['option'] == "Pan Dulce")
+                getPanDulce();
+        ?>
     </body>
 </html>
