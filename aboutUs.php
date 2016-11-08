6d8e06cbe91f7e1db1d144c11df095a73588c0d3
Sonali Bharat <sonalibharat17@yahoo.com>
2016-11-08 04:09:35 +0000

    fixing and cleaning issues

--- aboutUs.php
+++ aboutUs.php
@@ -14,6 +14,9 @@
     <li> <a href="aboutUs.php" > About Us </a>  </li>
     
    <li> <a href="https://docs.google.com/a/csumb.edu/document/d/15Y2a8pX6osQWlWS9bxPRVzYsgM_ioskDdpTHeaWm_MQ/edit?usp=sharing">Our Story</a> </li>
+
+   <li> <a href="https://docs.google.com/a/csumb.edu/spreadsheets/d/1zmK0NZRVcsTlGi0fY6Sw-T0EkgshVkOJYEnP03SQSIs/edit?usp=sharing">Database schema</a> </li>
+
 </ul>
     </body>
 </html>
--- index.php
+++ index.php
@@ -15,6 +15,8 @@
     <li> <a href="aboutUs.php" > About Us </a>  </li>
     
    <li> <a href="https://docs.google.com/a/csumb.edu/document/d/15Y2a8pX6osQWlWS9bxPRVzYsgM_ioskDdpTHeaWm_MQ/edit?usp=sharing">Our Story</a> </li>
+   <li> <a href="https://docs.google.com/a/csumb.edu/spreadsheets/d/1zmK0NZRVcsTlGi0fY6Sw-T0EkgshVkOJYEnP03SQSIs/edit?usp=sharing">Database schema</a> </li>
+
 </ul>
     
     <h2>Thank you for visiting our site! Take a look at our handmade products in the Menu section</h2>
